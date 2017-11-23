6b820bc52dc16b12ac74e31735f2db0930d65010
Artiom Nistrean <anistrean@andigital.com>
2017-11-23 14:26:23 +0000

    chore: building again...

--- dist/renders/externalBlock/package/libs/renderTypes.js
+++ dist/renders/externalBlock/package/libs/renderTypes.js
@@ -1,16 +1,16 @@
 var renderTypes = {
-    "blog": "3f6a5662-1998-4cf0-b369-f8ee6a7f0e42",
-    "image": "f0b1f9c8-9fbd-4470-aeb2-543c3cb01f19",
-    "text": "c6fe6238-4752-47bc-bc5e-3413d0d08d30",
-    "banner": "1dc05e15-e2cb-4b7a-84d6-354bc31aae7b",
+    "blog": "d3598cca-aa8c-4d5e-9c1d-d8606bed5da4",
+    "text": "e8d1864e-99d7-4357-b500-f16768f74136",
     "link": "7b908316-30d9-409e-8ff7-c23fa8526cfe",
-    "video": "eac8a783-932e-4900-bd96-bcaa84646499",
-    "slider": "7940d31f-513c-405b-b7b8-f60e6af8420a",
-    "snippet": "52a76880-f196-4e4e-9153-a32bcd24015f",
-    "card": "8b7fa2a3-2dee-494d-9eeb-0a46deaca253",
-    "splitBlock": "9d8300df-e2a5-44da-82c0-76a768b246ec",
+    "video": "f68430dd-4265-44cd-acbf-432e56537ad8",
+    "snippet": "29c72e17-6f40-4e55-bb02-0e5de7e74f66",
     "promoBanner": "4abaf1ef-79a4-4963-b020-25314ad1cfef",
     "externalBlock": "c0e08561-496e-415c-9858-2c34ec635513",
-    "cardList": "54e44352-19ac-49ed-b42a-2f2dc0bf771a",
-    "homepage": "0f979a5f-67e7-49b5-9b19-7490fac44db6"
+    "splitBlock": "27a0c147-900d-441b-9273-fe341a4a6729",
+    "slider": "47c162f5-8e3a-49f0-ae07-9cbb62cb316f",
+    "homepage": "a59a1f10-372d-4d2f-86fd-fdc8a042336e",
+    "image": "e8d1864e-99d7-4357-b500-f16768f74136",
+    "banner": "a8839424-4011-42eb-abf7-4d71e7b576cd",
+    "card": "8cfc9849-ad8d-4bb4-aa70-7b57e48c1837",
+    "cardList": "ffc19ac8-edaf-4f58-a02f-cb4d104fde5b"
 };
